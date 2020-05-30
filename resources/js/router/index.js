6bc4d98dea2658fac0a5347a2679e974f6b71d41
Christian <rvc_2595@hotmail.com>
2020-05-30 16:44:42 -0500

    JWT authentication and template integration

---
+++ resources/js/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  scrollBehavior() {
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  },
+  routes: [
+    // Dashboards
+
+    {
+      path: "/",
+      name: "analytics",
+      component: () => import("../DemoPages/Dashboards/Analytics.vue"),
+    },
+
+    // Pages
+
+    {
+      path: "/pages/login",
+      name: "login",
+      meta: { layout: "userpages" },
+      component: () => import("../DemoPages/UserPages/Login.vue"),
+    },
+  ],
+});
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="icon" href="<%= BASE_URL %>favicon.ico">
+    <title>ArchitectUI - Vuejs & Bootstrap 4 & Vuetify Admin UI Dashboard Template</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'ArchitectUI - Vuejs & Bootstrap 4 & Vuetify Admin UI Dashboard Template') }}</title>
+
+    <link href=js/chunk-20ec5cf3.c4085717.js rel=prefetch>
+    <link href=js/chunk-38256e16.98f863c6.js rel=prefetch>
+    <link href=js/chunk-d885080e.92f15431.js rel=prefetch>
+    <link href=css/app.b1d7ce4f.css rel=preload as=style>
+    <link href=css/chunk-vendors.e07fa558.css rel=preload as=style>
+    <link href=js/app.336507a9.js rel=preload as=script>
+    <link href=js/chunk-vendors.685c847a.js rel=preload as=script>
+    <link href=css/chunk-vendors.e07fa558.css rel=stylesheet>
+    <link href=css/app.b1d7ce4f.css rel=stylesheet>
+</head>
+
+<body>
+    <div id="app">
+    </div>
+
+    <script src=js/chunk-vendors.685c847a.js></script>
+    <script src=js/app.336507a9.js></script> 
+</body> 
+</html>
