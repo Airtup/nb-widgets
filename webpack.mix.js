e9b287d97a0d783edf6eb0f5eb6b69d81c60dfa9
Christian Llontop <christianllontop@icloud.com>
2020-05-30 18:45:02 -0500

    Fixing animate scss references

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,3 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+mix.js('resources/js/main.js', 'public/js')
+.sass('resources/sass/app.scss', 'public/css');
