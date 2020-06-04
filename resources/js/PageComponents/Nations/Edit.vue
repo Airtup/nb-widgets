c5d885c713498f16aa47c6d6b99ebf7757651a6a
Christian Llontop <christianllontop@icloud.com>
2020-06-04 02:25:52 -0500

    Typo changes

--- resources/js/PageComponents/Nations/Edit.vue
+++ resources/js/PageComponents/Nations/Edit.vue
@@ -314,7 +314,7 @@ export default {
         this.syncStatus = 1;
         this.syncCount = 0;
         this.updateSyncMembers("/api/v1/people?limit=50");
-      } else swal("Error", "Select a hq to sync", "warning");
+      } else swal("Error", "Select a membership to sync", "warning");
     },
     updateSyncMembers: function(url) {
       axios
