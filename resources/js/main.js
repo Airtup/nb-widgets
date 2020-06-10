8d40dc99188c7571f013d8c48731be476e6354c4
Christian Llontop <christianllontop@icloud.com>
2020-06-10 02:22:06 -0500

    Adding sentry on front

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,7 +8,10 @@ import App from "./App";
 
 import Default from "./Dashboard/baseLayout.vue";
 import VueHighlightJS from 'vue-highlightjs'
- 
+
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
+
 // Tell Vue.js to use vue-highlightjs
 Vue.use(VueHighlightJS)
 Vue.config.productionTip = false;
@@ -16,6 +19,11 @@ Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
 Vue.component("default-layout", Default);
+
+Sentry.init({
+    dsn: 'https://c879329a48764d1484fdb458015f0715@o404846.ingest.sentry.io/5271087',
+    integrations: [new VueIntegration({Vue, attachProps: true})],
+});
 
 new Vue({
   el: "#app",
