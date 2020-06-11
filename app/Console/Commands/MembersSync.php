944d0d1d3f8050ef0d5377eba1357b96f482154b
Bernardo Figueroa Hernandez <bernardofigueroa@iMac-de-Bernardo.local>
2020-06-11 14:51:43 -0500

    correcciones generales

--- app/Console/Commands/MembersSync.php
+++ app/Console/Commands/MembersSync.php
@@ -11,6 +11,7 @@ use App\Models\People;
 use App\Models\NationDetails;
 use App\Models\Nation;
 use Log;
+use Exception;
 
 class MembersSync extends Command
 {
@@ -123,58 +124,17 @@ class MembersSync extends Command
 
 
     public function updateMatchPerson($nation_id,$person_info,$nation_hq_id){
-
-        $org_person_id = $person_info->id;
-        $sub_nation = $this->dao->first($nation_id);
-
-
-        $org_membership_url = 'https://'.$sub_nation->slug.'.nationbuilder.com/api/v1/people/'.$org_person_id.'/memberships?limit=200&access_token='.$sub_nation->access_token;
-
-        $cookiesIn = '';
-        $curl = curl_init();
-        $options = array(
-            CURLOPT_URL => $org_membership_url,
-            CURLOPT_RETURNTRANSFER => true,     // return web page
-            // CURLOPT_HEADER         => true,     //return headers in  addition to content
-            CURLOPT_FOLLOWLOCATION => true,     // follow redirects
-            CURLOPT_ENCODING       => "",       // handle all encodings
-            CURLOPT_AUTOREFERER    => true,     // set referer on redirect
-            CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
-            CURLOPT_TIMEOUT        => 120,      // timeout on response
-            // CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
-            CURLINFO_HEADER_OUT    => true,
-            CURLOPT_SSL_VERIFYPEER => true,     // Validate SSL Certificates
-            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-            CURLOPT_COOKIE         => $cookiesIn
-        );
-        curl_setopt_array($curl, $options);
-        $curlResponse = curl_exec($curl);
-
-        $membership_results = json_decode($curlResponse);
-        $memberships = $membership_results->results;
+        try{
+            $org_person_id = $person_info->id;
+            $sub_nation = $this->dao->first($nation_id);
 
 
-        if (count($memberships) != 0) {
+            $org_membership_url = 'https://'.$sub_nation->slug.'.nationbuilder.com/api/v1/people/'.$org_person_id.'/memberships?limit=200&access_token='.$sub_nation->access_token;
 
-            $hq_nation = $this->dao->first($nation_hq_id);
-
-            $url = 'https://'.$hq_nation->slug.'.nationbuilder.com/api/v1/people/search?access_token='.$hq_nation->access_token;
-
-            if ($person_info->first_name != null && $person_info->first_name != '') {
-                $url.='&first_name='.$person_info->first_name;
-            }
-            if ($person_info->last_name != null && $person_info->last_name != '') {
-                $url.='&last_name='.$person_info->last_name;
-            }
-            if ($person_info->email != null && $person_info->email != '') {
-                $url.='&email='.$person_info->email;
-            }
-
-            $url = $this->encodeURI($url);
-
+            $cookiesIn = '';
             $curl = curl_init();
             $options = array(
-                CURLOPT_URL => $url,
+                CURLOPT_URL => $org_membership_url,
                 CURLOPT_RETURNTRANSFER => true,     // return web page
                 // CURLOPT_HEADER         => true,     //return headers in  addition to content
                 CURLOPT_FOLLOWLOCATION => true,     // follow redirects
@@ -189,65 +149,113 @@ class MembersSync extends Command
                 CURLOPT_COOKIE         => $cookiesIn
             );
             curl_setopt_array($curl, $options);
-            $peopleCurl = curl_exec($curl);
+            $curlResponse = curl_exec($curl);
 
-            $people = json_decode($peopleCurl);
+            $membership_results = json_decode($curlResponse);
+            $memberships = $membership_results->results;
 
-            $person_hq_id = 0;
-            if (count($people->results) == 1) {
-                $person_hq_id = $people->results[0]->id;
-            } else {
-                foreach ($people->results as $person) {
-                    if ($person->phone == $person_info->phone && $person->mobile == $person_info->mobile) {
-                        $person_hq_id = $person->id;
-                        break;
-                    }
+
+            if (count($memberships) != 0) {
+
+                $hq_nation = $this->dao->first($nation_hq_id);
+
+                $url = 'https://'.$hq_nation->slug.'.nationbuilder.com/api/v1/people/search?access_token='.$hq_nation->access_token;
+
+                if ($person_info->first_name != null && $person_info->first_name != '') {
+                    $url.='&first_name='.$person_info->first_name;
                 }
-            }
+                if ($person_info->last_name != null && $person_info->last_name != '') {
+                    $url.='&last_name='.$person_info->last_name;
+                }
+                if ($person_info->email != null && $person_info->email != '') {
+                    $url.='&email='.$person_info->email;
+                }
 
-            foreach ($memberships as $org_membership) {
-                $name           = isset($org_membership->name) ? $org_membership->name : '';
-                $status         = isset($org_membership->status) ? $org_membership->status : '';
-                $status_reason  = isset($org_membership->status_reason) ? $org_membership->status_reason : '';
-                $started_at     = isset($org_membership->started_at) ? $org_membership->started_at : '';
-                $expires_on     = isset($org_membership->expires_on) ? $org_membership->expires_on : '';
-
-                $params['membership']['name'] = $name; 
-                $params['membership']['status'] = $status; 
-                $params['membership']['status_reason'] = $status_reason; 
-                $params['membership']['started_at'] = $started_at; 
-                $params['membership']['expires_on'] = $expires_on; 
-
-
-                $url = 'https://'.$hq_nation->nation_slug.'.nationbuilder.com/api/v1/people/'.$person_hq_id.'/memberships?access_token='.$hq_nation->nation_auth;
+                $url = $this->encodeURI($url);
 
                 $curl = curl_init();
-
-                curl_setopt_array($curl, array(
+                $options = array(
                     CURLOPT_URL => $url,
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => "",
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => "PUT",
-                    CURLOPT_POSTFIELDS => json_encode($params),
-                    CURLOPT_HTTPHEADER => array(
-                        "x-rapidapi-host: google-translate1.p.rapidapi.com",
-                        "x-rapidapi-key: d1ec636ac5msh35885a399298175p14f2e4jsn92997c76c589",
-                        "Content-Type: application/json"
-                    ),
-                ));
+                    CURLOPT_RETURNTRANSFER => true,     // return web page
+                    // CURLOPT_HEADER         => true,     //return headers in  addition to content
+                    CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+                    CURLOPT_ENCODING       => "",       // handle all encodings
+                    CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+                    CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+                    CURLOPT_TIMEOUT        => 120,      // timeout on response
+                    // CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+                    CURLINFO_HEADER_OUT    => true,
+                    CURLOPT_SSL_VERIFYPEER => true,     // Validate SSL Certificates
+                    CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_COOKIE         => $cookiesIn
+                );
+                curl_setopt_array($curl, $options);
+                $peopleCurl = curl_exec($curl);
 
-                $memberCurl = curl_exec($curl);
+                $people = json_decode($peopleCurl);
 
-                Log::info(json_decode($memberCurl));
+                $person_hq_id = 0;
+                if (count($people->results) == 1) {
+                    $person_hq_id = $people->results[0]->id;
+                } else {
+                    foreach ($people->results as $person) {
+                        if ($person->phone == $person_info->phone && $person->mobile == $person_info->mobile) {
+                            $person_hq_id = $person->id;
+                            break;
+                        }
+                    }
+                }
+
+                foreach ($memberships as $org_membership) {
+                    $name           = isset($org_membership->name) ? $org_membership->name : '';
+                    $status         = isset($org_membership->status) ? $org_membership->status : '';
+                    $status_reason  = isset($org_membership->status_reason) ? $org_membership->status_reason : '';
+                    $started_at     = isset($org_membership->started_at) ? $org_membership->started_at : '';
+                    $expires_on     = isset($org_membership->expires_on) ? $org_membership->expires_on : '';
+
+                    $params['membership']['name'] = $name; 
+                    $params['membership']['status'] = $status; 
+                    $params['membership']['status_reason'] = $status_reason; 
+                    $params['membership']['started_at'] = $started_at; 
+                    $params['membership']['expires_on'] = $expires_on; 
+
+
+                    $url = 'https://'.$hq_nation->nation_slug.'.nationbuilder.com/api/v1/people/'.$person_hq_id.'/memberships?access_token='.$hq_nation->nation_auth;
+
+                    $curl = curl_init();
+
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => $url,
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => "",
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => "PUT",
+                        CURLOPT_POSTFIELDS => json_encode($params),
+                        CURLOPT_HTTPHEADER => array(
+                            "x-rapidapi-host: google-translate1.p.rapidapi.com",
+                            "x-rapidapi-key: d1ec636ac5msh35885a399298175p14f2e4jsn92997c76c589",
+                            "Content-Type: application/json"
+                        ),
+                    ));
+
+                    $memberCurl = curl_exec($curl);
+
+                    Log::info('--Log CURL Begin--');
+                    Log::info(json_decode($memberCurl));
+                    Log::info('--Log CURL End--');
+                }
+
+                return true;
+            }else{
+                return false;
             }
-
-            return true;
-        }else{
-            return false;
+        }catch(Exception $e){
+            Log::info('--Log Error Begin--');
+            Log::info($e->getMessage());
+            Log::info('--Log Error End--');
         }
     }
 
